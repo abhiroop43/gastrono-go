52e9c127c18431e3b5d0c4647f2b4a1739cfc65c
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-11-24 20:02:28 +0530

    feat(food): implement food fetching and creation functionality

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,32 +1,58 @@
 package controllers
 
 import (
+	"context"
+	"gastrono-go/database"
+	"gastrono-go/models"
 	"math"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
 
 func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		foodId := c.Param("food_id")
+		var food models.Food
 
+		err := foodCollection.FindOne(ctx, bson.M{"food_id": foodId}).Decode(&food)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while fetching the food"})
+		}
+		c.JSON(http.StatusOK, food)
 	}
 }
 
 func GetFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		var menu models.Menu
+		var food models.Food
 
+		if err := c.BindJSON(&food); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusAccepted, nil)
 	}
 }
 
 func CreateFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
 	}
 }
 
 func UpdateFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
 	}
 }
 
--- routes/foodRouter.go
+++ routes/foodRouter.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	 "gastrono-go/controllers"
+	"gastrono-go/controllers"
 
 	"github.com/gin-gonic/gin"
 )
