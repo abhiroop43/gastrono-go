625400e6861e551abb080c24cd745065cc9be22a
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-11-27 08:47:59 +0530

    refactor: improve foodController and menuController

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"gastrono-go/database"
 	"gastrono-go/models"
 	"math"
@@ -9,13 +10,23 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
+var (
+	menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
+	foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+	validate                         = validator.New()
+)
 
 func GetFoods() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func GetFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 		foodId := c.Param("food_id")
@@ -25,13 +36,14 @@ func GetFoods() gin.HandlerFunc {
 		defer cancel()
 
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while fetching the food"})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while fetching the food details"})
 		}
+
 		c.JSON(http.StatusOK, food)
 	}
 }
 
-func GetFood() gin.HandlerFunc {
+func CreateFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 		var menu models.Menu
@@ -42,12 +54,21 @@ func GetFood() gin.HandlerFunc {
 			return
 		}
 
-		c.JSON(http.StatusAccepted, nil)
-	}
-}
+		validationErr := validate.Struct(food)
 
-func CreateFood() gin.HandlerFunc {
-	return func(c *gin.Context) {
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		err := menuCollection.FindOne(ctx, bson.M{"menu_id": food.Menu_id}).Decode(&menu)
+		defer cancel()
+
+		if err != nil {
+			msg := fmt.Sprintf("Menu was not found")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
 	}
 }
 
--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,27 +1,40 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"gastrono-go/models"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GetMenus() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
 	}
 }
 
 func GetMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		var menu models.Menu
+		var food models.Food
 
+		if err := c.BindJSON(&food); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusAccepted, nil)
 	}
 }
 
 func CreateMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
 	}
 }
 
 func UpdateMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
 	}
 }
