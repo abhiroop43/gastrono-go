ebffd6f8af99f7635b8c24fed8857dd3c709d515
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-12-16 10:46:32 +0400

    feat(userController): add GetUser and SignUp functionalities

--- controllers/userController.go
+++ controllers/userController.go
@@ -2,10 +2,15 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"gastrono-go/database"
+	"gastrono-go/helpers"
+	"gastrono-go/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
 	"strconv"
@@ -59,21 +64,157 @@ func GetUsers() gin.HandlerFunc {
 }
 
 func GetUser() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		userId := c.Param("user_id")
+		var user models.User
+
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while listing the user"})
+		}
+		c.JSON(http.StatusOK, user)
+	}
 }
 
 func SignUp() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+
+		// bind the request payload to the model
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			defer cancel()
+			return
+		}
+
+		// validate the data
+		validationErr := validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			defer cancel()
+			return
+		}
+
+		// check if user email is already registered
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while checking if user is already registered"})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "user email already registered"})
+			return
+		}
+
+		// check if user phone number is already registered
+		count, err = userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while checking if user is already registered"})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "user phone number already registered"})
+			return
+		}
+
+		// generate a hashed password
+		password := HashPassword(*user.Password)
+		user.Password = &password
+
+		// generate user id and add time stamps
+		user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.UserId = user.ID.Hex()
+
+		// generate token and refresh token
+		token, refreshToken, _ := helpers.GenerateAllTokens(*user.Email, *user.FirstName, *user.LastName, user.UserId)
+		user.Token = &token
+		user.RefreshToken = &refreshToken
+
+		// insert user model into database
+		_, insertErr := userCollection.InsertOne(ctx, user)
+		if insertErr != nil {
+			msg := fmt.Sprintf("Failed to register user")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		defer cancel()
+
+		// return success message
+		c.JSON(http.StatusCreated, user)
+	}
 }
 
 func Login() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		var foundUser models.User
+
+		// bind the request payload to the model
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			defer cancel()
+			return
+		}
+
+		// check if user with this email exists
+		err := userCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid username or password"})
+			return
+		}
+
+		// verify the password
+		check, msg := VerifyPassword(*foundUser.Password, *user.Password)
+		defer cancel()
+		if !check {
+			c.JSON(http.StatusBadRequest, gin.H{"error": msg})
+			return
+		}
+
+		// generate token and refresh token
+		token, refreshToken, _ := helpers.GenerateAllTokens(*foundUser.Email, *foundUser.FirstName, *foundUser.LastName, foundUser.UserId)
+
+		// update user model with new tokens
+		helpers.UpdateAllTokens(token, refreshToken, foundUser.UserId)
+
+		// return success message
+		c.JSON(http.StatusOK, gin.H{"token": token, "refresh_token": refreshToken})
+	}
 }
 
+// HashPassword generate a hashed password
 func HashPassword(password string) string {
-	return ""
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Panicf(err.Error())
+	}
+
+	return string(bytes)
 }
 
-func VerifyPassword(hashedPassword, password string) bool {
-	return false
+// VerifyPassword Verify the password by comparing it with the user's hashed password
+func VerifyPassword(hashedPassword string, password string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	check := true
+	msg := ""
+
+	if err != nil {
+		msg = fmt.Sprintf("Either the username or the password is incorrect")
+		check = false
+	}
+
+	return check, msg
 }
